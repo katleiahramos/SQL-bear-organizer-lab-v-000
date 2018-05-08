a3ce44696335851f4b474e4213eae96aade7557d
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 16:53:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT into bears (name, age, gender, color, temperament, alive) VALUES ('Mr Chocolate', 10, 'M', 'Brown', 'good', TRUE)
+INSERT into bears (name, age, gender, color, temperament, alive) VALUES ('Mr Chocolate', 10, 'M', 'Brown', 'good')
