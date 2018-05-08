30a6c86a9d214457bf6c11d36140e35a25be92f4
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 17:23:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_most_prominent_color_and_returns_with_count
                              FROM bears
                              GROUP BY color
                             ) tmp
-                        )"
+                        );"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
